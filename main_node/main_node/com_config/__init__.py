cd8f8039fcd7888d9c6988c071f7db80d9dc3742
lxj <lixj_zj@163.com>
2019-06-09 21:41:44 +0800

    update common config position; update log

---
+++ main_node/main_node/com_config/__init__.py
@@ -0,0 +1,13 @@
+"""
+ !/usr/bin/env python3.6
+ -*- coding: utf-8 -*-
+ --------------------------------
+ Description :
+ --------------------------------
+ @Time    : 2019/6/8 23:35
+ @File    : __init__.py
+ @Software: PyCharm
+ --------------------------------
+ @Author  : lixj
+ @contact : lixj_zj@163.com
+"""
